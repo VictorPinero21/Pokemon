a81a303eb52ff2a59e0ae25a2c6863d6c4582f33
Victor Pinero Ollero <victorpinero21@gmail.com>
2025-02-05 15:15:17 +0100

    Home route and error 404 fixed

--- mi-proyecto/src/App.jsx
+++ mi-proyecto/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CardDetails from './pages/cardDetail';
 import Sets from './pages/sets';
 import Error404 from './components/error404';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Route path="/card/:id" element={<CardDetails />} />
     <Route path="/sets" element={<Sets />} />
     <Route path="*" element={<Error404 />} />
+    <Route path="/" element={<Home />} />
   </Routes>
   );
 }
---
+++ mi-proyecto/src/pages/home.jsx
@@ -0,0 +1,13 @@
+
+function CardDetails() {
+
+    return (
+        <div>
+         <h1>hola
+            
+         </h1>
+        </div>
+      );
+    }
+    
+    export default CardDetails;
